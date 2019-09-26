32a4b45e483f00a4ce522d5c90ec548de4f6ae00
Hugo Gonzalez <hugo.gonzalez@upslp.edu.mx>
2019-09-26 17:33:36 -0500

    Perform basic feature extraction

---
+++ BasicFlow.py
@@ -0,0 +1,144 @@
+#! /bin/env python
+# coding=utf-8
+# Copyright (C) 2019 CRI-Lab @ UPSLP
+# This file is part of pyFlowMeter - CRI-Lab @ UPSLP
+"""
+This module create and update the flows
+
+It also create consistent headers for the results
+
+"""
+import dpkt
+from dpkt.ethernet import Ethernet #pylint: disable=import-error
+from dpkt.ip import IP #pylint: disable=import-error
+from dpkt.ip6 import IP6 #pylint: disable=import-error
+from dpkt.icmp import ICMP #pylint: disable=import-error
+from dpkt.icmp6 import ICMP6 #pylint: disable=import-error
+from dpkt.tcp import TCP #pylint: disable=import-error
+from dpkt.udp import UDP #pylint: disable=import-error
+from dpkt.sctp import SCTP #pylint: disable=import-error
+
+import numpy as np
+
+import communityid
+
+intvalues = "fwPackets,\
+bwPackets,\
+"   
+
+npvalues = "flSize,\
+fwSize,\
+bwSize\
+"
+
+npfvalues = "flIAT,\
+fwIAT,\
+bwIAT\
+"
+
+values = "timestamp,\
+cid,\
+src,\
+dst,\
+sport,\
+dport,\
+proto,\
+fwPackets,\
+bwPackets,\
+"   
+
+
+# BasicFlow manipulations:
+
+def InitFlow( cid,tstamp,tpl,pkt):
+    """ InitFlow will create the basic structure to store a flow information"""
+    newFlow ={}
+    newFlow['timestamp'] = tstamp
+    newFlow['cid'] = cid
+    newFlow['tpl'] = tpl
+    newFlow['proto'] = tpl.proto
+    newFlow['orig'] = tpl.saddr
+    newFlow['bsrc'] = tpl.saddr
+    newFlow['bdst'] = tpl.daddr
+    newFlow['src'] = tpl._addr_to_ascii(tpl.saddr)
+    newFlow['dst'] = tpl._addr_to_ascii(tpl.daddr)
+    newFlow['sport'] = tpl.sport
+    newFlow['dport'] = tpl.dport
+    newFlow['lastseen'] = tstamp
+    newFlow['fwlastseen'] = tstamp
+    newFlow['bwlastseen'] = -1
+    #for sv in stringvalues.split(','):
+    #    newFlow[sv] = ''
+    for iv in intvalues.split(','):
+        newFlow[iv] = 0
+    
+    for nv in npvalues.split(','):
+        newFlow[nv] = np.array([], dtype=np.int64)
+    
+    for fv in npfvalues.split(','):
+        newFlow[fv] = np.array([], dtype=np.float64)
+    
+    return newFlow
+        
+        
+    
+#FlowUpdate(cFlow, tstamp,tpl,pkt)
+def FlowUpdate(newFlow, tstamp,tpl,pkt) :
+    
+    if IP in pkt:
+            saddr = pkt[IP].src
+            daddr = pkt[IP].dst
+            leng = pkt[IP].len
+    elif IP6 in pkt:
+            saddr = pkt[IP6].src
+            daddr = pkt[IP6].dst
+            leng =  pkt[IP6].plen
+    
+    if newFlow['orig'] == saddr:
+        newFlow['fwPackets'] += 1
+        newFlow['fwSize'] = np.append(newFlow['fwSize'], leng)
+        newFlow['fwIAT'] = np.append(newFlow['fwIAT'],tstamp - newFlow['fwlastseen'])
+        newFlow['fwlastseen'] = tstamp
+    else:
+        newFlow['bwPackets'] += 1
+        newFlow['bwSize'] = np.append(newFlow['bwSize'],leng)
+        if newFlow['fwlastseen'] > 0 :
+           newFlow['bwIAT'] = np.append(newFlow['bwIAT'],tstamp - newFlow['bwlastseen']) 
+        else:
+           newFlow['bwlastseen'] = tstamp
+        newFlow['bwlastseen'] = tstamp    
+    
+    newFlow['flSize'] = np.append(newFlow['flSize'],leng)
+    newFlow['flIAT'] = np.append(newFlow['flIAT'],tstamp - newFlow['lastseen'])
+    newFlow['lastseen'] = tstamp
+    
+
+def printHeaders():
+    """ Print the headers for the features """
+    strs = ''
+    for kv in values.split(','):
+        strs += kv + ','
+    for kv in npvalues.split(','):
+        strs += kv+"_sum,"
+        strs += kv+"_max,"
+        strs += kv+"_min,"
+        strs += kv+"_mean,"
+        strs += kv+"_median,"
+        strs += kv+"_std,"
+    
+
+def printFlow(Flow):
+    strs = ''
+    for kv in values.split(','):
+        strs += str(Flow[kv]) + ','
+    for kv in npvalues.split(','):
+        if Flow[kv].size > 0:
+            strs += str(np.sum(Flow[kv])) + ','
+            strs += str(np.max(Flow[kv])) + ','
+            strs += str(np.min(Flow[kv])) + ','
+            strs += str(np.mean(Flow[kv])) + ','
+            strs += str(np.median(Flow[kv])) + ','
+            strs += str(np.std(Flow[kv])) + ','
+        else:
+            strs += "0.0,"*6
+    print(strs)
