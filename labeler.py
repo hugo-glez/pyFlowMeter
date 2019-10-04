65c0fa36f90cc7519c6c30c3c3a8b23a2eb7e706
hugo-glez <hugo.glez@gmail.com>
2019-10-03 19:52:17 -0500

    Managing labels from flow community id

--- labeler.py
+++ labeler.py
@@ -10,17 +10,18 @@ This module is a helper to obtain info for labels
 labelArray = {}
 
 def loadLabels(lfile='labels.txt'):
-    try:
+    #try:
         with open(lfile,'r') as fs:
             info = fs.read().split('/n')
-        for k,v in info.split(','):
+        for kv in info:
+            k,v = kv.split(',')
             labelArray[k] = v
-    except:
-        labelArray['n'] = 'a'
+    #except:
+    #    labelArray['n'] = 'a'
 
 
 def getLabel(cid):
     if len(labelArray) == 0:
         loadLabels()
-    r = labelArray.get(cid,"None")
+    r = labelArray.get(cid,"NoneLabel")
     return r
